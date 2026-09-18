8e1b23fda85a79dbaf0c92d52947709f65c3255f
Duziks <3078802823@qq.com>
2026-09-18 17:25:00 +0800

    add v2.13.0

---
+++ v2.13.0/inductor_npu/test_op_completeness.py
@@ -0,0 +1,55 @@
+import torch
+import torch_npu
+from torch_npu.contrib import transfer_to_npu
+from torch_npu.utils import _dynamo
+_dynamo.use_jit_script = True
+torch.cuda.get_device_capability = lambda :(10, 0)
+import torch_npu.testing
+
+# Owner(s): ["module: inductor"]
+import unittest
+
+from torch._inductor.codegen.cpp import CppOverrides, CppVecOverrides
+from torch._inductor.codegen.halide import HalideOverrides
+from torch._inductor.codegen.mps import MetalOverrides
+from torch._inductor.codegen.triton import TritonKernelOverrides
+from torch._inductor.ops_handler import list_ops, OP_NAMES, OpsHandler
+from torch._inductor.test_case import TestCase
+import torch_npu._inductor
+
+
+class TestOpCompleteness(TestCase):
+    def verify_ops_handler_completeness(self, handler):
+        for op in OP_NAMES:
+            self.assertIsNot(
+                getattr(handler, op),
+                getattr(OpsHandler, op),
+                msg=f"{handler} must implement {op}",
+            )
+        extra_ops = list_ops(handler) - OP_NAMES
+        if extra_ops:
+            raise AssertionError(
+                f"{handler} has an extra ops: {extra_ops}, add them to OpHandler class or prefix with `_`"
+            )
+
+    def test_triton_overrides(self):
+        self.verify_ops_handler_completeness(TritonKernelOverrides)
+
+    def test_cpp_overrides(self):
+        self.verify_ops_handler_completeness(CppOverrides)
+
+    def test_cpp_vec_overrides(self):
+        self.verify_ops_handler_completeness(CppVecOverrides)
+
+    def test_halide_overrides(self):
+        self.verify_ops_handler_completeness(HalideOverrides)
+
+    @unittest.skip("MPS backend not yet finished")
+    def test_metal_overrides(self):
+        self.verify_ops_handler_completeness(MetalOverrides)
+
+
+if __name__ == "__main__":
+    from torch._inductor.test_case import run_tests
+
+    run_tests()
